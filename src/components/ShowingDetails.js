1cd63911692e0170ccf281d9bba66b5a64b2b72b
rajatjhamnani <rajatjhamnani@gmail.com>
2023-12-08 01:53:07 +0530

    three hours react project

--- src/components/ShowingDetails.js
+++ src/components/ShowingDetails.js
@@ -1,41 +1,42 @@
 import React from "react";
 
 const ShowingDetails = (props) => {
+  // Group details by table
+  const detailsByTable = {};
+  props.details.forEach((detail) => {
+    const table = detail.selectedTable;
+    console.log(table);
+    if (!detailsByTable[table]) {
+      detailsByTable[table] = [];
+    }
+    detailsByTable[table].push(detail);
+    console.log(detailsByTable);
+  });
+
+  // Define all three tables
+  const allTables = ["Table 1", "Table 2", "Table 3"];
+
   return (
     <div className="App">
-      <table>
-        <thead>
-          <tr>
-            <th>Table 1</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.details.selectedTable === "Table 1" &&
-            props.details.map((detail) => {
-              <tr>
-                <td>
-                  {detail.selectedDish}
-                  {detail.selectedPrice}
-                </td>
-              </tr>;
-            })}
-        </tbody>
-      </table>
-      <table>
-        <thead>
-          <tr>
-            <th>Table 2</th>
-          </tr>
-        </thead>
-      </table>
-      <table>
-        <thead>
-          <tr>
-            <th>Table 3</th>
-          </tr>
-        </thead>
-      </table>
+      <h2>Entered Details:</h2>
+      {allTables.map((table, index) => (
+        <div key={index}>
+          <h3>{table}</h3>
+          <ul>
+            {detailsByTable[table] ? (
+              detailsByTable[table].map((detail, idx) => (
+                <li key={idx}>
+                  Dish: {detail.selectedDish}, Price: {detail.selectedPrice}
+                </li>
+              ))
+            ) : (
+              <li>No details entered yet</li>
+            )}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
+
 export default ShowingDetails;
